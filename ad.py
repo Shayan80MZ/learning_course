8fa5a6f83b0d22629ca403fb81fe70b0c947a40e
Shayan80MZ <shayan80sama@yahoo.com>
2021-02-24 19:34:08 +0330

    create Advertiser, Ad and BaseAdvertising classes without help and describeMe methods

---
+++ ad.py
@@ -0,0 +1,31 @@
+from base_model import BaseAdvertising
+
+
+class Ad(BaseAdvertising):
+    def __init__(self, id, title, imgUrl, link, advertiser):
+        super().__init__(id)
+        self.__title = title
+        self.__imgUrl = imgUrl
+        self.__link = link
+        self.__advertiser = advertiser
+
+    def getTitle(self):
+        return self.__title
+
+    def setTitle(self, title):
+        self.__title = title
+
+    def getImgUrl(self):
+        return self.__imgUrl
+
+    def setImgUrl(self, imgUrl):
+        self.__imgUrl = imgUrl
+
+    def getLink(self):
+        return self.__link
+
+    def setLink(self, link):
+        self.__link = link
+
+    def setAdvertiser(self, advertiser):
+        self.__advertiser = advertiser
---
+++ advertiser.py
@@ -0,0 +1,27 @@
+from base_model import BaseAdvertising
+
+
+class Advertiser(BaseAdvertising):
+    allAdvertisers = set()
+
+    def __init__(self, id, name):
+        super().__init__(id)
+        self.__name = name
+        Advertiser.allAdvertisers.add(self)
+
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
+
+    def help(self):
+        pass
+
+    @staticmethod
+    def getTotalClicks():
+        clicksList = list()
+        for advertiser in Advertiser.allAdvertisers:
+            clicksList.append(advertiser.getClicks())
+
+        return sum(clicksList)
